be25f5c6b7b20a94b0069906c291f7e81a10adb6
Rixafy <rixafy@gmail.com>
2019-10-12 15:34:40 +0200

    Add plugin author to annotations

--- src/main/kotlin/net/minecord/beautyindicator/BeautyIndicator.kt
+++ src/main/kotlin/net/minecord/beautyindicator/BeautyIndicator.kt
@@ -12,11 +12,13 @@ import org.bukkit.plugin.java.annotation.command.Commands
 import org.bukkit.plugin.java.annotation.plugin.Description
 import org.bukkit.plugin.java.annotation.plugin.Plugin
 import org.bukkit.plugin.java.annotation.plugin.Website
+import org.bukkit.plugin.java.annotation.plugin.author.Author
 
 @Plugin(name = "BeautyIndicator", version = "1.8")
 @Description("Minecraft (Spigot/Bukkit) plugin for indicating mob health")
 @Commands(Command(name = "net/minecord/beautyindicator", desc = "Help command"))
 @Website("https://minecord.net")
+@Author("Rixafy")
 class BeautyIndicator : JavaPlugin() {
     private val pluginPrefix = "&b[BeautyIndicator] &7"
     lateinit var combatController: CombatController
