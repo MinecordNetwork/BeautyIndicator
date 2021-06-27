c0080bf6cb01f28eee41c5eba019007639c8df64
Rixafy <rixafy@gmail.com>
2021-06-27 22:19:45 +0200

    Bump version

--- src/main/kotlin/net/minecord/beautyindicator/BeautyIndicator.kt
+++ src/main/kotlin/net/minecord/beautyindicator/BeautyIndicator.kt
@@ -14,7 +14,7 @@ import org.bukkit.plugin.java.annotation.plugin.Plugin
 import org.bukkit.plugin.java.annotation.plugin.Website
 import org.bukkit.plugin.java.annotation.plugin.author.Author
 
-@Plugin(name = "BeautyIndicator", version = "1.10")
+@Plugin(name = "BeautyIndicator", version = "1.11")
 @Description("Minecraft (Spigot/Bukkit) plugin for indicating mob health")
 @Commands(Command(name = "beautyindicator", desc = "Help command"))
 @Website("https://minecord.net")
