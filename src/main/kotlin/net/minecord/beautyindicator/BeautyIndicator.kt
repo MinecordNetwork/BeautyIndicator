3dcee409a0aaf217c2d825f538f9d7d65d7ebeea
Rixafy <rixafy@gmail.com>
2021-07-03 16:42:44 +0200

    Remove spigot annotations

--- src/main/kotlin/net/minecord/beautyindicator/BeautyIndicator.kt
+++ src/main/kotlin/net/minecord/beautyindicator/BeautyIndicator.kt
@@ -7,18 +7,7 @@ import net.minecord.beautyindicator.controller.WorldController
 import net.minecord.beautyindicator.listener.CombatListener
 import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
-import org.bukkit.plugin.java.annotation.command.Command
-import org.bukkit.plugin.java.annotation.command.Commands
-import org.bukkit.plugin.java.annotation.plugin.Description
-import org.bukkit.plugin.java.annotation.plugin.Plugin
-import org.bukkit.plugin.java.annotation.plugin.Website
-import org.bukkit.plugin.java.annotation.plugin.author.Author
 
-@Plugin(name = "BeautyIndicator", version = "1.11")
-@Description("Minecraft (Spigot/Bukkit) plugin for indicating mob health")
-@Commands(Command(name = "beautyindicator", desc = "Help command"))
-@Website("https://minecord.net")
-@Author("Rixafy")
 class BeautyIndicator : JavaPlugin() {
     private val pluginPrefix = "&b[BeautyIndicator] &7"
     lateinit var combatController: CombatController
